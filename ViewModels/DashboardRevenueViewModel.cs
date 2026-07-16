3eaa6a832f60a84667d5eb49d4207001a4ac7ce6
tuanduy-wq <tuanduyla@gmail.com>
2026-07-16 10:24:00 +0700

    chore: update DashboardRevenueViewModel for revenue statistics

--- ViewModels/DashboardRevenueViewModel.cs
+++ ViewModels/DashboardRevenueViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace Bus_ticket.ViewModels;
+﻿using System;
+using System.Collections.Generic;
 
-public class DashboardRevenueViewModel
+namespace Bus_ticket.ViewModels
 {
-    
+    public class DashboardRevenueViewModel
+    {
+        public List<RevenueByCategoryDto> ChartData { get; set; } = new();
+        public List<TransactionDetailDto> TableData { get; set; } = new();
+    }
+
+    public class RevenueByCategoryDto
+    {
+        public string Category { get; set; } = string.Empty;
+        public decimal TotalRevenue { get; set; }
+    }
+
+    public class TransactionDetailDto
+    {
+        public string BookingCode { get; set; } = string.Empty;
+        public string CustomerName { get; set; } = string.Empty;
+        public string BusClass { get; set; } = string.Empty;
+        public DateTime PaymentDate { get; set; }
+        public decimal Amount { get; set; }
+    }
 }
